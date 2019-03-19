7bc145da3f8af3465c96e8cecd28b3f191e45309
rasheed-rasoon <rasheed.rasoon@gmail.com>
2019-03-19 21:27:23 +0530

    recent commit

--- SeleniumPracticeMvnGitJen/src/test/java/CollectionsSample/HashMapSample.java
+++ SeleniumPracticeMvnGitJen/src/test/java/CollectionsSample/HashMapSample.java
@@ -1,6 +1,7 @@
 package CollectionsSample;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map.Entry;
 
@@ -37,12 +38,65 @@ for(Entry<String,Integer> entry2:lHashMap.entrySet())
 	System.out.println(entry2);
 }
 
-	}
+System.out.println("key value pairs are lements are ");
 
+for(Entry<String,Integer> entry2:lHashMap.entrySet())
+	
+{
+	System.out.println(entry2.getKey() +" : "+entry2.getValue());
+}
+
+	}
+	
+	public static void HashMapIterations()
+	{
+		HashMap<String,Integer> hMap=new HashMap<String, Integer>();
+		hMap.put("Rasheed",8);
+		hMap.put("ahmed",6);
+
+
+		hMap.put("sheik",21);
+
+
+		hMap.put("Boby",2);
+		
+		System.out.println("iterate using Iterator ");
+		
+		Iterator itr=hMap.entrySet().iterator();
+		
+		while(itr.hasNext())
+		{
+			System.out.println(itr.next());
+		}
+	}
+public static void hashMapToArrayList()
+{
+	HashMap<String,Integer> hMap=new HashMap<String, Integer>();
+	hMap.put("Rasheed",8);
+	hMap.put("shiva",6);
+
+
+	hMap.put("karthik",21);
+
+
+	hMap.put("Balu",2);
+	
+	
+	
+	
+	
+
+
+
+}
 
 public static void main(String args[])
+
 {
-	HashMapSample.hashMapAddElements();	
+	//HashMapSample.hashMapAddElements();	
+	HashMapSample.HashMapIterations();
+	
 }
 
 }
+
